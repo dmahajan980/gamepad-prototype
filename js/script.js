5c12433cc1e2410b7d7c5b149a38acb3763db370
dmahajan980 <divyanshumahajan98@gmail.com>
2020-02-25 03:48:07 +0530

    Refactored code and added functionality to read buttons on click.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,58 @@
-window.addEventListener("gamepadconnected", function(e) {
-    console.log(`Gamepad connected at index ${e.gamepad.index}: ${e.gamepad.id}, ${e.gamepad.buttons.length} buttons, ${e.gamepad.axes.length} axes.`);
+let message, gamepad, connectivityInterval, buttonsData;
+
+const buttonPressed = key => (typeof key == 'object') ? key.pressed : false;
+
+window.addEventListener("gamepadconnected", function() {
+    gamepad = navigator.getGamepads()[0];
+    console.log(gamepad)
+
+    message = `${gamepad.id} <br> ${gamepad.buttons.length} buttons <br> ${gamepad.axes.length} axes`;
+    document.querySelector('.console-message').innerHTML = message;
+
+    connectivityInterval = setInterval(runGamepad, 100);
 });
 
 window.addEventListener("gamepaddisconnected", function(e) {
-    console.log(`Gamepad disconnected from index ${e.gamepad.index}: ${e.gamepad.id}`);
+    if (e.gamepad.index === gamepad.index) {
+        clearInterval(connectivityInterval);
+        message = `Gamepad disconnected`;
+        document.querySelector('.console-message').innerHTML = message;
+    }
 });
 
-console.log(navigator.getGamepads());
+// Perform actions/functionality using gamepad
+function runGamepad() {
+    let gamepadObject = navigator.getGamepads()[0];
+    let buttons = gamepadObject.buttons;
+    let axes = gamepadObject.axes;
+
+    let div = document.createElement('div');
+    div.classList = 'values';
+    
+    for (let i = 0; i < buttons.length; i++) {
+        let p = document.createElement('p');
+
+        // When button is pressed
+        if (buttons[i].value > 0.4) {
+            console.log(buttons[i]);        
+            p.innerHTML = `Button ${i} pressed`;
+        }
+        div.appendChild(p);
+    }
+
+    for (let j = 0; j < axes.length; j++) {
+        // When button is pressed
+        if (axes[j].value > 0.4) {
+            console.log(axes[j]);        
+            // p.innerHTML = `Button ${i} pressed`;
+        }
+    }
+
+    /*
+     * A way to add new data to previous
+     */
+    // Saving buttons data
+    // buttonsData = buttons;
+
+    document.querySelector('.value-box').replaceChild(div, document.querySelector('.values'));
+}
