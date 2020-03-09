4d78a6405ded9c58ce8c3ddaecc19e3e92242da0
dmahajan980 <divyanshumahajan98@gmail.com>
2020-03-09 16:34:46 +0530

    Attempt to add tab functionality

--- js/script.js
+++ js/script.js
@@ -107,3 +107,25 @@ const keyHandler = buttonIndex => {
             break;
     }
 }
+
+const focusNextElement = () => {
+    //add all elements we want to include in our selection
+    var focussableElements = 'a:not([disabled]), button:not([disabled]), input[type=text]:not([disabled]), [tabindex]:not([disabled]):not([tabindex="-1"])';
+    if (document.activeElement && document.activeElement.form) {
+        var focussable = Array.prototype.filter.call(document.activeElement.form.querySelectorAll(focussableElements),
+            function (element) {
+                //check for visibility while always include the current activeElement 
+                return element.offsetWidth > 0 || element.offsetHeight > 0 || element === document.activeElement
+            });
+        var index = focussable.indexOf(document.activeElement);
+        if (index > -1) {
+            var nextElement = focussable[index + 1] || focussable[0];
+            nextElement.focus();
+        }
+    }
+}
+
+// {
+//     let focussableElements = 'a:not([disabled]), button:not([disabled]), input[type=text]:not([disabled]), [tabindex]:not([disabled]):not([tabindex="-1"])';
+
+// }
